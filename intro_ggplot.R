b5b543941d01f2b35a101d328f6502345cf6d8c6
Mariana Monteiro <marianamsva@gmail.com>
2023-09-17 15:53:13 -0300

    Atividade Análise Exploratória de Dados

---
+++ intro_ggplot.R
@@ -0,0 +1,14 @@
+pacman::p_load(corrplot, dplyr, ggplot2)
+
+# BASE DE DADOS IRIS SEM AS ESPÉCIES #
+iris
+
+# Gráfico de Caixas Multivariado
+
+iris %>% ggplot(aes(y = Sepal.Length, x = Species)) + geom_boxplot()
+
+# Gráfico de Barras - Contagem de Espécies
+barplot(table(iris$Species), main = "Contagem de Espécies", xlab = "Espécies", ylab = "Contagem",)  # Usando cores diferentes para as três espécies
+
+# Gráfico de dispersão 
+iris %>% ggplot(aes(x = Species, y = Sepal.Width)) + geom_point()
